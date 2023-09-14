876b275359e41a9cc5f6a278eca4675be0c6ec30
ychistel <54749745+ychistel@users.noreply.github.com>
2023-09-14 19:35:35 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -53,7 +53,7 @@ exclude_patterns = []
 #html_theme = 'alabaster'
 #html_theme = 'sphinx_rtd_theme'
 html_theme = 'sphinx_book_theme'
-
+html_title = '1 NSI'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
