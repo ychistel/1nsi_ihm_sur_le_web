36313b5622f31609ad0deeee8284ac3135c6ffbe
ychistel <ychistel@ac-caen.fr>
2023-09-14 19:33:45 +0200

    modification de conf.py et ajout de _static

--- source/conf.py
+++ source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Interface Homme Machine - IHM'
+project = '1NSI'
 copyright = '2022, Yannick Chistel'
 author = 'Yannick Chistel'
 
@@ -27,10 +27,8 @@ author = 'Yannick Chistel'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-'nbsphinx',
-]
-
+#extensions = ['nbsphinx','sphinx.ext.githubpages',]
+extensions = []
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -53,15 +51,17 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['../_static']
+html_static_path = ['_static']
 
 html_css_files = ['custom.css']
 
+"""
 html_theme_options = {
     # Toc options
     'collapse_navigation': True,
@@ -70,3 +70,4 @@ html_theme_options = {
     'includehidden': True,
     'titles_only': False
 }
+"""
